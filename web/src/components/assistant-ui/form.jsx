7b71f96bc59ee7d85753d354e19165b9e6d26a30
aZeDen <lucasgosselin3101@gmail.com>
2026-03-30 17:13:03 +0200

    Ajout des noms des input

--- web/src/components/assistant-ui/form.jsx
+++ web/src/components/assistant-ui/form.jsx
@@ -7,10 +7,10 @@ export function Form() {
             <h2>⚠ Je ne trouve pas de réponse fiable</h2>
 
             <form method="POST" className="no-response-form">
-                <input type="text" placeholder="Nom" />
-                <input type="text" placeholder="Prénom" />
-                <input type="text" placeholder="Classe" />
-                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Nom" name="last_name" required />
+                <input type="text" placeholder="Prénom" name="first_name" required />
+                <input type="text" placeholder="Classe" name="class" required />
+                <input type="text" placeholder="Email" name="email" required />
                 <button type="submit">Envoyer à la péda</button>
             </form>
         </div>
